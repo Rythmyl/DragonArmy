01f879fb36a0328d24799385bbb2d8301a0f32e4
Tristan Ramirez <Tsramirez@student.fullsail.edu>
2025-12-14 21:41:45 -0500

    credits music
    Added a script to play music on the credits scene, and made and added credits music

---
+++ Assets/Scripts/creditsMusic.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class creditsMusic : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        if (audioManager.Instance != null)
+        {
+            audioManager.Instance.PlayCreditsMusic();
+        }
+    }
+}
